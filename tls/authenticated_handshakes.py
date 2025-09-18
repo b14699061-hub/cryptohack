d5c8e9351924d576dcadb9787740aa35df5695de
Your Name <your_email@example.com>
2025-09-18 11:59:44 +0300

    tls

--- tls/authenticated_handshakes.py
+++ tls/authenticated_handshakes.py
@@ -1,21 +1,41 @@
-from scapy.all import rdpcap
-from scapy.layers.tls.all import TLS, TLSServerHello, encry
+from scapy.all import rdpcap, conf
+from scapy.layers.tls.all import TLS, TLSServerHello, TLSClientHello, TCP
+from scapy.layers.tls import handshake
 
-PCAP_FILE_PATH = "tls/res/no-finished-tls3_642a73844a64e902ef6c8564972e98ca.cryptohack.org.pcapng"
+PCAP_FILE_PATH = "tls/res/no-finished-tls3.cryptohack.org.pcapng"
+SSL_KEYS_FILE_PATH = "tls/res/keylogfile.txt"
 
 def run():
+
+    # Enable TLS decryption globally
+    conf.tls_session_enable = True
+    conf.tls_nss_filename = SSL_KEYS_FILE_PATH
+
     packets = rdpcap(PCAP_FILE_PATH)
 
-    server_hello = None
+    print(len(packets))
+
+    server_hello: TLSServerHello | None = None
+    client_hello: TLSClientHello = None
 
     for pkt in packets:
         if pkt.haslayer(TLS):
             tls = pkt[TLS]
-            if tls.msg and isinstance(tls.msg[0], TLSServerHello):
-                print(tls.msg[0].summary())
-                print(type(tls.msg[0]))
 
+            if tls.msg and isinstance(tls.msg[0], TLSServerHello):
                 server_hello = tls.msg[0]
+                print(server_hello.summary())
+
+            elif tls.msg and isinstance(tls.msg[0], TLSClientHello):
+                client_hello = tls.msg[0]
+                print(client_hello.summary())
+
+            else:
+                print("#" * 10 + "\n")
+                print(tls.summary())
+
+        else:
+            print(pkt.summary())
 
 if __name__ == "__main__":
     run()
